9ba8db2eb1aad0b25d73e804a865966cfcbea1dd
Jan Klimo <jan.klimo@gmail.com>
2017-09-08 10:35:33 +0700

    add blacklisted symbols

--- app/lib/dip_detector.rb
+++ app/lib/dip_detector.rb
@@ -3,6 +3,8 @@ require 'httparty'
 class DipDetector
   attr_reader :coin_data
 
+  BLACKLIST = %w(MIOTA XEM)
+
   def initialize
     resp = HTTParty.get("https://api.coinmarketcap.com/v1/ticker/?limit=10")
     @coin_data = JSON.parse(resp.body)
@@ -13,8 +15,11 @@ class DipDetector
       change_1h = hash['percent_change_1h'].to_f
       change_24h = hash['percent_change_24h'].to_f
       change_7d = hash['percent_change_7d'].to_f
+      symbol = hash['symbol']
 
-      (change_1h > 0) && (change_24h < ENV.fetch('TRIGGER_24H').to_f) &&
+      !symbol.in?(BLACKLIST) &&
+        (change_1h > 0) &&
+        (change_24h < ENV.fetch('TRIGGER_24H').to_f) &&
         (change_7d < ENV.fetch('TRIGGER_7D').to_f)
     end
   end
